6bf28cd3dff99cdd8659f06f6058ee7ffc6ec9a5
Crowdbotics <team@crowdbotics.com>
2020-09-19 17:59:34 +0000

    Update models

--- backend/teachers_text_20447/wsgi.py
+++ backend/teachers_text_20447/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teachers_text_20447.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teachers_text_20447.settings")
 
 application = get_wsgi_application()
